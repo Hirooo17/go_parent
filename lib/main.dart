0cd1f93e812c3e80e962e48aa04086ff7d8e3fd2
Aeron Bulatao <bulataoaeronjay@gmail.com>
2024-09-30 18:51:22 +0800

    TEST add display text inside scaffold widget

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,11 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(child: Text("asd"),),
+      ),
+    );
   }
 }
 
