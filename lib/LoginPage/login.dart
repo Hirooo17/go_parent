11464841ec18ca1f50cdf272396cc189a828dc59
Aeron Bulatao <bulataoaeronjay@gmail.com>
2024-10-03 02:15:22 +0800

    add responsiveness to loginpage

--- lib/LoginPage/login.dart
+++ lib/LoginPage/login.dart
@@ -8,6 +8,7 @@ import 'package:go_parent/Widgets/text_field.dart';
 import 'package:go_parent/Widgets/button.dart';
 import 'package:go_parent/authentication/auth.dart';
 import 'package:go_parent/Widgets/snackbar.dart';
+import 'package:go_parent/Widgets/responsive.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -17,13 +18,12 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-
-   final TextEditingController emailController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
   bool isLoading = false;
+  final double mobileSize = 700;
 
-
-    @override
+  @override
   void dispose() {
     super.dispose();
     emailController.dispose();
@@ -58,91 +58,106 @@ class _LoginPageState extends State<LoginPage> {
     }
   }
 
-
-void byPass() {
-  // You don't need setState here, as Navigator.push does not require it
-  Navigator.push(
-    context,
-    MaterialPageRoute(builder: (context) => Homescreen()),
-  );
-}
-
-
-
-  
-
+  void byPass() {
+    // You don't need setState here, as Navigator.push does not require it
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => Homescreen()),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     double height = MediaQuery.of(context).size.height;
     return Scaffold(
-      resizeToAvoidBottomInset : false,
-
-
-
-
-
-
-
-
-
-
-      
+      resizeToAvoidBottomInset: false,
       backgroundColor: Colors.white,
       body: SafeArea(
-          child: SizedBox(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            SizedBox(
-              width: double.infinity,
-              height: height / 2.7,
-              child: Image.asset('images/login.jpg'),
+          child: Row(
+        children: [
+          if(!Responsive.isMobile(context))
+            Expanded(
+                flex: Responsive.isTablet(context) ? 2 : 1,
+                child: Container(
+                    decoration: BoxDecoration(
+                  color: Colors.blue, // Set the background color
+                ))),
+          Expanded(
+            flex: 3,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              
+              children: [
+                SizedBox(
+                  width: double.infinity,
+                  height: height / 2.7,
+                  child: Image.asset('images/login.jpg'),
+                ),
+
+                SizedBox(
+                  width: mobileSize,
+                  child: TextFieldInput(
+                      icon: Icons.person,
+                      textEditingController: emailController,
+                      hintText: 'Enter your email',
+                      textInputType: TextInputType.text),
+                ),
+
+                SizedBox(
+                  width: mobileSize,
+                  child: TextFieldInput(
+                    icon: Icons.lock,
+                    textEditingController: passwordController,
+                    hintText: 'Enter your passord',
+                    textInputType: TextInputType.text,
+                    isPass: true,
+                  ),
+                ),
+
+                //  we call our forgot password below the login in button
+                SizedBox(
+                  width: mobileSize,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 35),
+                    child: Align(
+                      alignment: Alignment.centerRight,
+                      child: Text(
+                        "Forgot Password",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                            color: Colors.blue),
+                      ),
+                    ),
+                  ),
+                ),
+
+                // bypass added for debugging purposes
+                SizedBox(child: MyButtons(onTap: byPass, text: "login"), width: mobileSize,),
+                SizedBox(height: height / 15),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Dont Have an account?"),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (context) => Signup()));
+                      },
+                      child: Text(
+                        "Sign Up",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 16),
+                      ),
+                    )
+                  ],
+                )
+
+                //text field
+              ],
             ),
-
-            TextFieldInput(
-                icon: Icons.person,
-                textEditingController: emailController,
-                hintText: 'Enter your email',
-                textInputType: TextInputType.text),
-
-                
-
-            TextFieldInput(
-              icon: Icons.lock,
-              textEditingController: passwordController,
-              hintText: 'Enter your passord',
-              textInputType: TextInputType.text,
-              isPass: true,
-            ),
-
-             //  we call our forgot password below the login in button
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 35),
-              child: Align(
-                alignment: Alignment.centerRight,
-                child: Text("Forgot Password", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16, color: Colors.blue),),),
-            ),
-            
-
-            // bypass added for debugging purposes
-            MyButtons(onTap: byPass, text: "login"),
-            SizedBox(height: height/15),
-           Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text("Dont Have an account?"),
-              GestureDetector(onTap: (){
-              Navigator.push(context, MaterialPageRoute(builder: (context) => Signup()));
-                
-              },child: Text("Sign Up", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),),)
-            ],
-           )
-            
-            
-            //text field
-          ],
-        ),
+          ),
+        ],
       )),
     );
   }
