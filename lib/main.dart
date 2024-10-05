8f3f736195d486e5234ed9da6e0ada9838ffd347
Hirooo17 <retuermahero17@gmail.com>
2024-10-06 02:44:07 +0800

    roar

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,10 @@
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:go_parent/LoginPage/login.dart';
+import 'package:flutter/services.dart';
+
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/material.dart';
+
 import 'package:go_parent/LoginPage/login_screen.dart';
 import 'package:go_parent/LoginPage/signup_screen.dart';
 import 'package:go_parent/Screen/home_screen.dart';
