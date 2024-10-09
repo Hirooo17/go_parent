6505da07b24331f9399901935d5950df3f4e129e
Hirooo17 <retuermahero17@gmail.com>
2024-10-09 16:06:14 +0800

    Introduction Screen no back end yet

---
+++ lib/Screen/introduction_screen.dart
@@ -0,0 +1,85 @@
+// ignore_for_file: prefer_final_fields, avoid_unnecessary_containers
+
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/extension_navigation.dart';
+import 'package:go_parent/LoginPage/signup_screen.dart';
+import 'package:go_parent/intro%20screens/intro_screens_1.dart';
+import 'package:go_parent/intro%20screens/intro_screens_2.dart';
+import 'package:go_parent/intro%20screens/intro_screens_3.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+
+class IntroductionScreen extends StatefulWidget {
+  const IntroductionScreen({super.key});
+
+  @override
+  State<IntroductionScreen> createState() => _IntroductionScreenState();
+}
+
+class _IntroductionScreenState extends State<IntroductionScreen> {
+// controller to keep track our page
+  PageController _controller = PageController();
+  bool onLastPage = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          PageView(
+            onPageChanged: (index) {
+              setState(() {
+                onLastPage = (index == 2);
+              });
+            },
+            controller: _controller,
+            children: [
+              IntroScreens1(),
+              IntroScreens2(),
+              IntroScreens3(),
+            ],
+          ),
+
+          // dot indicator
+          Container(
+              alignment: Alignment(0, 0.90),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  // skip
+                  GestureDetector(
+                      onTap: () {
+                        _controller.jumpToPage(2);
+                      },
+                      child: Text('skip')),
+
+                  SmoothPageIndicator(controller: _controller, count: 3),
+
+                  // next or done
+                  onLastPage ?
+                  GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context, 
+                          MaterialPageRoute(
+                            builder: (context){
+                              return Signup();
+                            }));
+                      },
+                      child: Text('done')
+                      
+                      )
+                      : GestureDetector(
+                      onTap: () {
+                        _controller.nextPage(
+                            duration: Duration(milliseconds: 500),
+                            curve: Curves.easeIn);
+                      },
+                      child: Text('next')), 
+                      
+                ],
+              )),
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:go_parent/LoginPage/login_screen.dart';
 import 'package:go_parent/LoginPage/signup_screen.dart';
 import 'package:go_parent/Screen/home_screen.dart';
+import 'package:go_parent/Screen/introduction_screen.dart';
 import 'package:go_parent/Screen/profile_screen.dart';
 import 'package:go_parent/Widgets/side_menu.dart';
 import 'firebase_options.dart';
@@ -36,13 +37,14 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        initialRoute: Signup.id,
-        routes: {
-          Homescreen.id: (context) => Homescreen(),
-          LoginPage.id: (context) => LoginPage(),
-          Signup.id: (context) => Signup(),
-          Logout.id: (context) => Logout(),
-        }
+        //initialRoute: Signup.id,
+        //routes: {
+        //  Homescreen.id: (context) => Homescreen(),
+         // LoginPage.id: (context) => LoginPage(),
+         // Signup.id: (context) => Signup(),
+         // Logout.id: (context) => Logout(),
+       // },
+        home: IntroductionScreen(),
         // StreamBuilder(stream: FirebaseAuth.instance.authStateChanges(), builder: (context, snapshot){
         //  if (snapshot.hasData){
         //     return Homescreen();
