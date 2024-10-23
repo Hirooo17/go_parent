4b18b15d23339b9f91093a705b8b613cff26868e
Aeron Bulatao <bulataoaeronjay@gmail.com>
2024-10-23 13:15:18 +0800

    add easy_faq pub package

--- lib/Screen/FAQ.dart
+++ lib/Screen/FAQ.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_easy_faq/flutter_easy_faq.dart';
 
 class Faq extends StatelessWidget {
   @override
@@ -8,7 +9,18 @@ class Faq extends StatelessWidget {
         title: Text("Frequently Asked Questions"),
       ),
       body: Center(
-        child: Text("FAQ content goes here"),
+        child: Padding(
+          padding: const EdgeInsets.all(
+              28.0), // You can adjust the padding as needed
+          child: Column(
+            children: [
+              EasyFaq(
+                question: "question?",
+                answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua\nUt enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
